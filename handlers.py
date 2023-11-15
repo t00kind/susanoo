bbb4ef3b9b9b3a5971e98ffb5bdd63d933051585
tonebula <absolutesdev@gmail.com>
2023-11-15 21:12:42 +0500

    batoni

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
+import kb
 
 
 router = Router()
@@ -13,4 +14,8 @@ async def start_handler(msg: Message):
 
 @router.message()
 async def message_handler(msg: Message):
-    await msg.answer(f"EBASH")
+    await msg.answer(f"EBASH RIGHT NOW BRUH", reply_markup=kb.menu)
+
+@router.message(F.text == "get it")
+async def menu(msg: Message):
+    await msg.answer("right", reply_markup=kb.menu)
---
+++ kb.py
@@ -0,0 +1,9 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+menu = [
+    [InlineKeyboardButton(text="СОЗИДАТЬ", callback_data="view")],
+    [InlineKeyboardButton(text="ТРАХАТЬ", callback_data="tofukk")],
+    [InlineKeyboardButton(text="МОТИВАЦИЮ ПОДНЯТЬ", callback_data="upm")]
+]
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="get it")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="get out", callback_data="menu")]])
