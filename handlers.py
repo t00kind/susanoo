84a5455679bf2ae9bc0b7ff35ab988e215d8f21b
tonebula <absolutesdev@gmail.com>
2024-02-06 14:07:45 +0500

    get dfsdfds

--- handlers.py
+++ handlers.py
@@ -3,6 +3,11 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 import kb
 
+a = "CgACAgQAAxkBAAOvZcH1VyLVHLxbp13bUYE09TOTMhwAAvcCAAKvXQ1T2t9JcNUxm0E0BA"
+b = "CgACAgQAAxkBAAOxZcH1Y71flGQs7P0zoRe3B5-xXdcAAscCAAKiaxxTr4L2eS0mCnY0BA"
+c = "CgACAgQAAxkBAAOzZcH1c9jVP_qgr9gBXssUOhF1YhgAAk4FAAKLbJxSyoXXsvHYnwM0BA"
+chmp = "CgACAgQAAxkBAAO5ZcH2pU0pMc9XijziRGvUEqNmJegAAgkDAAKszA1TuooR-V3Jc340BA"
+
 
 def ne():
     import datetime
@@ -17,6 +22,7 @@ def ne():
 router = Router()
 
 
+
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     await msg.answer("Привет! Я способен помочь любыми средствами")
@@ -24,22 +30,22 @@ async def start_handler(msg: Message):
 
 @router.message()
 async def message_handler(msg: Message):
-    await msg.answer(f"EBASH RIGHT NOW BRUH", reply_markup=kb.menu)
+    await msg.answer_animation(chmp, caption=f"EBASH RIGHT NOW BRUH", reply_markup=kb.menu)
 
 @router.callback_query(F.data == "upm")
 async def newy(call: CallbackQuery):
     ans = ne()
-    await call.message.answer(ans, reply_markup=kb.menu)
+    await call.message.answer_animation(c, caption=ans, reply_markup=kb.menu)
     await call.answer()
 @router.callback_query(F.data == "tofukk")
 async def newy(call: CallbackQuery):
     ans = "<b>ЕБАШИТЬ ПОРА БРАТ, ТРАХАТЬ ВСЕ ДЕРЬМО. ИДТИ К МЕЧТЕ</b>"
-    await call.message.answer(ans, reply_markup=kb.menu)
+    await call.message.answer_animation(a, caption=ans, reply_markup=kb.menu)
     await call.answer()
     
     
 @router.callback_query(F.data == "view")
 async def newy(call: CallbackQuery):
     ans = "<i>Красота есть во всем, не каждому суждено её увидеть</i>"
-    await call.message.answer(ans, reply_markup=kb.menu)
+    await call.message.answer_animation(b, caption=ans, reply_markup=kb.menu)
     await call.answer()
